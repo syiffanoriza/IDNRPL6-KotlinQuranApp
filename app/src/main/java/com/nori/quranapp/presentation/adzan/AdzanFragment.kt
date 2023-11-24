2cc9fc1823192c99b7383b3ad5fd36274898ce0c
syiffanoriza <syiffa.noriza@gmail.com>
2023-11-24 09:47:17 +0700

    Working on Adzan Feature UI

--- app/src/main/java/com/nori/quranapp/presentation/adzan/AdzanFragment.kt
+++ app/src/main/java/com/nori/quranapp/presentation/adzan/AdzanFragment.kt
@@ -1,21 +1,17 @@
 package com.nori.quranapp.presentation.adzan
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.nori.quranapp.R
 import com.nori.quranapp.databinding.FragmentAdzanBinding
 import com.nori.quranapp.network.Resource
 import com.nori.quranapp.presentation.ViewModelFactory
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AdzanFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AdzanFragment : Fragment() {
     private var _binding: FragmentAdzanBinding? = null
     private val binding get() = _binding as FragmentAdzanBinding
@@ -38,11 +34,55 @@ class AdzanFragment : Fragment() {
                 is Resource.Success -> {
                     binding.apply {
                         it.data?.let { adzanDataResult ->
-                            tvLocation
+                            tvLocation.text = adzanDataResult.listLocation[1]
+                            tvDate.text = adzanDataResult.listCalendar[3]
+                        }
+                    }
+                    when (val adzanTime = it.data?.dailyAdzan) {
+                        is Resource.Loading -> {}
+                        is Resource.Success -> {
+                            binding.apply {
+                                adzanTime.data?.let { time ->
+                                    tvTimeImsak.text = time.imsak
+                                    tvTimeFajr.text = time.fajr
+                                    tvTimeZuhr.text = time.zuhr
+                                    tvTimeAsr.text = time.asr
+                                    tvTimeMaghrib.text = time.maghrib
+                                    tvTimeIsha.text = time.isha
+                                }
+                            }
+                        }
+
+                        is Resource.Error -> {
+                            Log.e(
+                                "AdzanFragment",
+                                "Error getting schedule: ${adzanTime.message}"
+                            )
+                            Toast.makeText(context, "Error: ${adzanTime.message}", Toast.LENGTH_SHORT).show()
+                        }
+                        else -> {
+                            Log.e(
+                                "AdzanFragment",
+                                "Error getting location: ${it.message}"
+                            )
+                            Toast.makeText(context, "Error: ${it.message}", Toast.LENGTH_SHORT).show()
                         }
                     }
                 }
+
+                is Resource.Error -> {
+                    Log.e(
+                        "AdzanFragment",
+                        "Error observing AdzanViewModel: ${it.message}"
+                    )
+                    Toast.makeText(context, "Error: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
             }
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/nori/quranapp/utils/Const.kt
@@ -0,0 +1,3 @@
+package com.nori.quranapp.utils
+
+const val PERMISSION_LOC_REQ_CODE = 1000
